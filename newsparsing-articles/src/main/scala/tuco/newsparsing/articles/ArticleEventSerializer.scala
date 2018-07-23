7b6267c570f7812b8d226d302d1bc66b4dd71aab
Nicolas Ribeiro <ribeiro@safety-data.com>
2018-07-23 10:55:18 +0200

    Update article event serializer

--- newsparsing-articles/src/main/scala/tuco/newsparsing/articles/ArticleEventSerializer.scala
+++ newsparsing-articles/src/main/scala/tuco/newsparsing/articles/ArticleEventSerializer.scala
@@ -7,26 +7,26 @@ import scala.io.Codec
 
 class ArticleEventSerializer extends SerializerWithStringManifest {
 
-  val ArticleEventManifest = "article-event"
+  val ArticleEventManifest = classOf[ArticleEvent].getName
 
   def identifier: Int = 1414
 
   def manifest(o: AnyRef): String =
     o match {
       case _: ArticleEvent => ArticleEventManifest
-      case _               => throw new Exception(f"Unsupported type ${o.getClass}")
+      case _ => throw new Exception(f"Unsupported type ${o.getClass}")
     }
 
   def fromBinary(bytes: Array[Byte], manifest: String): AnyRef =
     manifest match {
-      case ArticleEventManifest => ???
-      case _                    => throw new Exception(f"Unsupported manifest $manifest")
+      case ArticleEventManifest => new String(bytes, Codec.UTF8.charSet).parseJson.convertTo[ArticleEvent]
+      case _ => throw new Exception(f"Unsupported manifest $manifest")
     }
 
   def toBinary(o: AnyRef): Array[Byte] =
     o match {
       case e: ArticleEvent => e.toJson.compactPrint.getBytes(Codec.UTF8.charSet)
-      case _               => throw new Exception(f"Unsupported type ${o.getClass}")
+      case _ => throw new Exception(f"Unsupported type ${o.getClass}")
     }
 
 }
